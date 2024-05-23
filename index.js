a52facb0e4c72b991f2b8fc7f659175d6978959a
Caio Camargo <caiocbahia@hotmail.com>
2024-05-23 19:53:07 -0300

    Added get/post for cars listing

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const app = express();
 
 const conn = require('./db/conn');
 
+// Models
+
+const Car = require('./models/Car');
+const CarController = require('./controllers/CarController');
+
 conn
   .sync()
   .then(() => {
     app.listen(3000);
   })
   .catch(err => console.log(err));
+
+  app.use(
+    express.urlencoded({
+      extended: true
+    })
+  );
+
+  app.use(express.json());
+
+  //routes
+
+  app.get('/carros', CarController.showAllCars);
+
+  app.post('/carros', CarController.createCar);
