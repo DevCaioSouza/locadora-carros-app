cddd55aeb6c53d63da6898979726575f689b5689
Caio Camargo <caiocbahia@hotmail.com>
2024-05-25 20:58:07 -0300

    add vercel.json file

--- routes/carRoutes.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const CarController = require('../controllers/CarController');
-
-// router.get('/carros', CarController.showAllCars);
-// router.get('/carros/disponiveis', CarController.availableCars);
-// router.post('/carros', CarController.createCar);
-
-// module.exports = router;
